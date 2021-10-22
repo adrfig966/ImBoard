333abd53f62a29de33790abeeb1ebff585cf1b65
adrfig966 <adrfigueroa@csumb.edu>
2021-10-21 22:11:31 -0700

    Installed PassportJS and Google OAuth2 extension. Installed cookie-sessions package. Installed dotenv package and setup env file for Google credentials. Set up authorization testing routes with working login and logout.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require("dotenv").config({path: __dirname + '/googleauth.env'});
+const passport = require('passport');
 const express = require("express");
+const cookieSession = require('cookie-session');
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const nunjucks = require("nunjucks");
@@ -11,6 +14,7 @@ const Post = require("./schemas/post");
 //Routes
 const templateroutes = require("./routes/templateroutes");
 const apiroutes = require("./routes/apiroutes");
+const authroutes = require("./routes/auth-test");
 
 //Custom Middleware
 const middleware = require("./util/middleware");
@@ -18,6 +22,12 @@ const middleware = require("./util/middleware");
 /**************
 Server & DB configuration
 ***************/
+
+//Session storage set up
+app.use(cookieSession({
+  name: 'session-name',
+  keys: ['key1', 'key2']
+}))
 
 //Mongoose connection set up
 mongoose.connect(
@@ -29,7 +39,7 @@ mongoose.connect(
 );
 mongoose.set("useFindAndModify", false);
 
-//Setting nunjucks as view engine
+//Setting Nunjucks as view engine
 nunjucks.configure("views", {
   autoescape: true, //Escape HTML automatically
   express: app,
@@ -50,11 +60,13 @@ app.use(cookieParser());
 app.use(bodyParser());
 app.use(middleware.grabIP);
 app.use(express.static("public"));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Router middleware
 app.use(templateroutes);
 app.use(apiroutes);
-
+app.use(authroutes);
 /**************
 Special Routes
 ***************/
---
+++ routes/auth-test.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    return cb(null, profile);
+  }
+));
+
+/* Testing purposes */
+router.get("/checkenv", (req, res) => {
+  console.log(process.env.GOOGLE_CLIENT_ID);
+  res.status(200).send("Ping");
+});
+
+router.get('/failed', (req, res) => {
+  res.send('<h1>Could not log in(</h1>')
+});
+
+// Middleware - Check user is Logged in
+const checkUserLoggedIn = (req, res, next) => {
+  req.user ? next(): res.sendStatus(401);
+}
+
+//Protected Route.
+router.get('/profile', checkUserLoggedIn, (req, res) => {
+  res.send(`<h1>${req.user.displayName}'s Dashboard</h1>`)
+});
+
+// Auth and callback routes
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    res.redirect('/profile');
+  }
+);
+
+//Logout
+router.get('/logout', (req, res) => {
+    req.session = null;
+    req.logout();
+    res.redirect('/');
+})
+
+module.exports = router;
